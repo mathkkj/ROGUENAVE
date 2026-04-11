8bd8bbff0d8d507b39000b14067f8ad64e8f4d82
mathkkj <matheus.cabral24@nave.org.br>
2026-04-10 22:26:52 -0300

    Update Dash

--- personagem.gd
+++ personagem.gd
@@ -1,8 +1,17 @@
 extends CharacterBody2D
 
-@export var velocidade = 350.0
-@export var aceleracao = 2750.0
-@export var atrito = 1500.0
+@export var velocidade: float = 350.0
+@export var aceleracao: float = 2750.0
+@export var atrito: float = 1500.0
+@export var dash_vel: float = 1300.0
+
+@export var DASH_TIMER: float = 0.10
+@export var tempo_recarregamento_dash: float = 1.0
+
+var pode_dash: bool = true
+var tempo_dash: float = 0.0
+var recarregar_tempo_dash: float = 0.0
+var dash_dir: Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	var direcao := Input.get_vector("esquerda", "direita", "cima", "baixo")
@@ -13,5 +22,32 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.move_toward(velocidade_alvo, aceleracao * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, atrito * delta)
-
+	
+	_mecanica_dash(delta)
 	move_and_slide()
+
+func _mecanica_dash(delta: float) -> void:
+	if pode_dash and Input.is_action_just_pressed("dash"):
+		pode_dash = false
+		tempo_dash = DASH_TIMER
+		recarregar_tempo_dash = tempo_recarregamento_dash
+		
+		#ACHEI RUIM BOTAR O ANALOGICO DIREITO OU O MOUSE PARA A DIREÇÃO DO DASH
+		#if Global.usando_controle == false:
+			#dash_dir = global_position.direction_to(get_global_mouse_position())
+		#else:
+			#dash_dir = Input.get_vector("esquerdaAnalogicoDireito", "direitaAnalogicoDireito", "cimaAnalogicoDireito", "baixoAnalogicoDireito")
+		dash_dir = Input.get_vector("esquerda", "direita", "cima", "baixo")
+		velocity = dash_dir * dash_vel
+
+		if dash_dir.x != 0:
+			$Sprite2D.flip_h = dash_dir.x > 0
+
+	if tempo_dash > 0.0:
+		tempo_dash = max(0.0, tempo_dash - delta)
+	else:
+		if recarregar_tempo_dash > 0.0:
+			recarregar_tempo_dash -= delta
+		else:
+			pode_dash = true
+	
