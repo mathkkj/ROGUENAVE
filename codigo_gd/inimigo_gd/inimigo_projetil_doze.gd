f03ac56c1c7b943c417751b61f5cf4529e3a3aab
mathkkj <matheus.cabral24@nave.org.br>
2026-05-31 18:12:16 -0300

    inimigo projetil variação (atira igual uma doze)

--- codigo_gd/inimigo_gd/inimigo_projetil_doze.gd
+++ codigo_gd/inimigo_gd/inimigo_projetil_doze.gd
@@ -7,14 +7,24 @@ func atirar():
 
 	estado_atual = ESTADOS.ATIRANDO
 	velocity = Vector2.ZERO
+	for i in range(3):
+		var projetil = projetil_instancia.instantiate()
+		projetil.global_position = global_position
 
-	var projetil = projetil_instancia.instantiate()
-	projetil.global_position = global_position
-	projetil.direcao = (alvo.global_position - projetil.global_position).normalized()
-	projetil.rotation = projetil.direcao.angle()
+		var direcao = (alvo.global_position - global_position).normalized()
 
-	get_tree().current_scene.add_child(projetil)
+		var spread = deg_to_rad(15) # 15 graus para cada lado
+		var angulo = (i - 1) * spread # -15, 0, +15
+
+		projetil.direcao = direcao.rotated(angulo)
+		projetil.rotation = projetil.direcao.angle()
+
+		get_tree().current_scene.add_child(projetil)
 
 	# Sai do estado de ataque depois do tempo do Timer
 	estado_atual = ESTADOS.CACANDO
 	atirar_tempo.start()
+
+
+func _on_hurtbox_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
