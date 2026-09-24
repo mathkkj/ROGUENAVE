7a5bfba976f044279242b09141b7b9abdb5f3b67
mathkkj <matheus.cabral24@nave.org.br>
2026-09-23 22:37:04 -0300

    pequena parte do codigo de geração procedural
    peguei a base do video do igor a alterei algumas coisas, fiz isso pra testar como seria a criação de novas cenas e tals
    
    eu preciso criar aquela variavel que guarda a ultima sala que eu entrei pra nao ficar gerando aleatorio toda vez q eu entrar e sair da sala que eu entrei antes

--- codigo_gd/teste_geracao_procedural_gd/raiz.gd
+++ codigo_gd/teste_geracao_procedural_gd/raiz.gd
@@ -4,6 +4,8 @@ var base = preload("res://cenas_tscn/teste_geracao_procedural_tscn/base.tscn")
 
 var mapa = null
 
+var personagem
+
 const lista_mapas = {
 	"direita": [
 		"sala1"
@@ -13,13 +15,58 @@ const lista_mapas = {
 	]
 }
 
+
 func _ready() -> void:
 	mapa = base.instantiate()
 	add_child(mapa)
-	
+
+	personagem = Global.personagem
+
+
+func verificar_direcao_saida() -> String:
+	var posicao = personagem.global_position
+
+	if posicao.x < 0:
+		return "esquerda"
+
+	if posicao.x > 560:
+		return "direita"
+
+	if posicao.y < 0:
+		return "cima"
+
+	if posicao.y > 315:
+		return "baixo"
+
+	return ""
+
+
+func _physics_process(_delta):
+	var direcao = verificar_direcao_saida()
+
+	if direcao != "":
+		carregar_mapa(direcao)
+
+
 func carregar_mapa(direcao):
 	mapa.queue_free()
+
 	var nome_mapa = lista_mapas[direcao].pick_random()
-	var carregar = load("res://cenas_tscn/teste_geracao_procedural_tscn/"+nome_mapa+".tscn")
+
+	var carregar = load("res://cenas_tscn/teste_geracao_procedural_tscn/" + nome_mapa + ".tscn")
+
 	mapa = carregar.instantiate()
 	add_child(mapa)
+
+	# coloca o jogador no lado oposto
+	if direcao == "direita":
+		personagem.global_position.x = 20
+
+	elif direcao == "esquerda":
+		personagem.global_position.x = 540
+
+	elif direcao == "cima":
+		personagem.global_position.y = 295
+
+	elif direcao == "baixo":
+		personagem.global_position.y = 20
